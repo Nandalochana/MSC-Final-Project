2e5337c5a08045a4e3be78ead03b15afd8fdfc4b
NandaLochana <danushkanandalochana@gmail.com>
2024-12-30 18:55:32 +0200

    configured homepage

--- client/client/src/components/App.tsx
+++ client/client/src/components/App.tsx
@@ -1,40 +1,20 @@
 import React from 'react';
-import { GoogleMap, LoadScript, Marker } from '@react-google-maps/api';
-import dotenv from 'dotenv';
-
-dotenv.config();
-
-const containerStyle = {
-  width: '100%',
-  height: '400px'
-};
-
-const center = {
-  lat: -3.745,
-  lng: -38.523
-};
-
-const locations = [
-  { lat: -3.745, lng: -38.523 },
-  { lat: -3.746, lng: -38.524 },
-  { lat: -3.747, lng: -38.525 },
-  { lat: -3.748, lng: -38.526 },
-  // Add more locations here
-];
+import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
+import MapPage from './MapPage';
 
 const App: React.FC = () => {
   return (
-    <LoadScript googleMapsApiKey={process.env.REACT_APP_GOOGLE_MAPS_API_KEY || ''}>
-      <GoogleMap
-        mapContainerStyle={containerStyle}
-        center={center}
-        zoom={10}
-      >
-        {locations.map((location, index) => (
-          <Marker key={index} position={location} />
-        ))}
-      </GoogleMap>
-    </LoadScript>
+    <Router>
+      <div>
+        <h1>Welcome to the App</h1>
+        <Link to="/map">
+          <button>Go to Map</button>
+        </Link>
+        <Switch>
+          <Route path="/map" component={MapPage} />
+        </Switch>
+      </div>
+    </Router>
   );
 };
 
